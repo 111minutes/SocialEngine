d146430de2a76b8a7b5f83f6775232bdecd0abcb
malaar <korshilovskiy@gmail.com>
2012-02-29 11:28:22 +0200

    prepare base structure

---
+++ SocialEngine/DXAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DXAppDelegate.h
+//  SocialEngine
+//
+//  Created by Malaar on 2/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DXViewController;
+
+@interface DXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) DXViewController *viewController;
+
+@end
---
+++ SocialEngine/DXViewController.h
@@ -0,0 +1,13 @@
+//
+//  DXViewController.h
+//  SocialEngine
+//
+//  Created by Malaar on 2/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DXViewController : UIViewController
+
+@end
